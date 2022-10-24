0f78e4c7d13e36b1edd5f819198d882ae3379e19
markonser <markonser@yandex.ru>
2022-10-24 22:36:21 +0300

    3.13. Маршрут по тайным тропам

--- project/src/components/card/card.tsx
+++ project/src/components/card/card.tsx
@@ -7,7 +7,7 @@ export default function Card(): JSX.Element {
       </div>
       <div className='cities__image-wrapper place-card__image-wrapper'>
         <a href='/'>
-          <img className='place-card__image' src='img/apartment-01.jpg' width='260' height='200' alt='Place image' />
+          <img className='place-card__image' src='img/apartment-01.jpg' width='260' height='200' />
         </a>
       </div>
       <div className='place-card__info'>
--- project/src/components/favorites/favorites.tsx
+++ project/src/components/favorites/favorites.tsx
@@ -50,7 +50,7 @@ export default function Favorites(): JSX.Element {
                     </div>
                     <div className='favorites__image-wrapper place-card__image-wrapper'>
                       <a href='/'>
-                        <img className='place-card__image' src='img/apartment-small-03.jpg' width='150' height='110' alt='Place image' />
+                        <img className='place-card__image' src='img/apartment-small-03.jpg' width='150' height='110'/>
                       </a>
                     </div>
                     <div className='favorites__card-info place-card__info'>
@@ -82,7 +82,7 @@ export default function Favorites(): JSX.Element {
                   <article className='favorites__card place-card'>
                     <div className='favorites__image-wrapper place-card__image-wrapper'>
                       <a href='/'>
-                        <img className='place-card__image' src='img/room-small.jpg' width='150' height='110' alt='Place image' />
+                        <img className='place-card__image' src='img/room-small.jpg' width='150' height='110'/>
                       </a>
                     </div>
                     <div className='favorites__card-info place-card__info'>
@@ -125,7 +125,7 @@ export default function Favorites(): JSX.Element {
                   <article className='favorites__card place-card'>
                     <div className='favorites__image-wrapper place-card__image-wrapper'>
                       <a href='/'>
-                        <img className='place-card__image' src='img/apartment-small-04.jpg' width='150' height='110' alt='Place image' />
+                        <img className='place-card__image' src='img/apartment-small-04.jpg' width='150' height='110'/>
                       </a>
                     </div>
                     <div className='favorites__card-info place-card__info'>
