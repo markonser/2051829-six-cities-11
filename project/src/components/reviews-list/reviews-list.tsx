5f76e3ecd854ae789dcdfb69459e38db701eb0aa
markonser <markonser@yandex.ru>
2022-11-12 14:34:44 +0300

    fixes

--- project/src/components/reviews-list/reviews-list.tsx
+++ project/src/components/reviews-list/reviews-list.tsx
@@ -1,19 +1,28 @@
 import { Comment } from '../../types/types';
 import RewiewItem from '../rewiew-item/rewiew-item';
+import CommentSendForm from '../../components/comment-send-form/comment-send-form';
 
 type Props = {
-  comment: Comment;
+  comments: Comment[];
 }
 
-export default function RewiewsList({ comment }: Props): JSX.Element {
+export default function RewiewsList({ comments }: Props): JSX.Element {
 
   return (
-    <ul className='reviews__list'>
-      <RewiewItem
-        comment={comment}
-      />
-    </ul >
+    <section className='property__reviews reviews'>
+      <div>
+        <h2 className='reviews__title'>Reviews &middot; <span className='reviews__amount'>{comments.length}</span></h2>
 
+        <ul className='reviews__list'>
+          {comments.map((comment) => (
+            <RewiewItem
+              comment={comment} key={comment.id}
+            />
+          ))}
+        </ul >
+      </div>
+      <CommentSendForm />
+    </section>
   );
 }
 
--- project/src/pages/property-page/property-page.tsx
+++ project/src/pages/property-page/property-page.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { Comment, Offer } from '../../types/types';
-import CommentSendForm from '../../components/comment-send-form/comment-send-form';
+
 import Header from '../../components/header/header';
 import RewiewsList from '../../components/reviews-list/reviews-list';
 import { Helmet } from 'react-helmet-async';
@@ -124,15 +124,10 @@ export default function Property({ offers, comments }: Props) {
                   </p>
                 </div >
               </div>
-              <section className='property__reviews reviews'>
-                <div>
-                  <h2 className='reviews__title'>Reviews &middot; <span className='reviews__amount'>{comments.length}</span></h2>
-                  {comments.map((comment) => (
-                    <RewiewsList comment={comment} key={comment.id} />
-                  ))}
-                </div>
-                <CommentSendForm />
-              </section>
+
+              <RewiewsList comments={comments} />
+
+
             </div>
           </div>
         </section>
@@ -156,40 +151,6 @@ export default function Property({ offers, comments }: Props) {
                 />
               ))
             }
-            {/* <article className='near-places__card place-card'>
-              <div className='place-card__mark'>
-                <span>Premium</span>
-              </div>
-              <div className='near-places__image-wrapper place-card__image-wrapper'>
-                <a href='/'>
-                  <img className='place-card__image' src='img/apartment-03.jpg' width='260' height='200' alt='' />
-                </a>
-              </div>
-              <div className='place-card__info'>
-                <div className='place-card__price-wrapper'>
-                  <div className='place-card__price'>
-                    <b className='place-card__price-value'>&euro;180</b>
-                    <span className='place-card__price-text'>&#47;&nbsp;night</span>
-                  </div>
-                  <button className='place-card__bookmark-button button' type='button'>
-                    <svg className='place-card__bookmark-icon' width='18' height='19'>
-                      <use xlinkHref='#icon-bookmark'></use>
-                    </svg>
-                    <span className='visually-hidden'>To bookmarks</span>
-                  </button>
-                </div>
-                <div className='place-card__rating rating'>
-                  <div className='place-card__stars rating__stars'>
-                    <span style={{ 'width': '100%' }}></span>
-                    <span className='visually-hidden'>Rating</span>
-                  </div>
-                </div>
-                <h2 className='place-card__name'>
-                  <a href='/'>Nice, cozy, warm big bed apartment</a>
-                </h2>
-                <p className='place-card__type'>Apartment</p>
-              </div>
-            </article> */}
           </div>
         </section>
       </div>
